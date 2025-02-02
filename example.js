737672441512e8437029d1fb441c0beed523413c
Annette <annetaliya@gmail.com>
2025-02-01 20:38:51 -0800

    add index of the maximum number

---
+++ example.js
@@ -0,0 +1,5 @@
+const array = [6, 8, 9, 4];
+
+for (let i = 0; i <array.length; i++) {
+    console.log(array[i], i);
+}
--- max.js
+++ max.js
@@ -2,19 +2,29 @@
 // the maximum number in the array.
 
 //Example array input : [3, 1, 7, 5, 9, 2] , the expected output is = 9
+//edge cases:1- check if the input is a valid array,
+//2 - If there a Mixture of array elements string and numbers, ignore non-numeric values
+
+//scenarion: [1, 5, "Hello", 6, , 9]
 
 function findMax(array) {
     if (array.length === 0) {
         return null
     }
+    if (!Array.isArray(array)) {
+        return 'Invalid Input';
+    }
+    
     let max = array[0];
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 1; i < array.length; i++) {
+        if (typeof array[i] !== "number") {
+            continue;
+        }
         if (array[i] > max) {
             max = array[i];
         }
     }
-    return max;
+    return `The Max value is ${max} Index: ${array.indexOf(max)}`;
 
 }
-console.log(findMax([]))
-
+console.log(findMax([1, 5, "Hello", 6,  9]))
