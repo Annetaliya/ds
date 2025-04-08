4469a8f004c6dae41d5ba5b912dc6dee5db8943c
Annette <annetaliya@gmail.com>
2025-04-07 21:58:50 -0700

    classes

--- class.js
+++ class.js
@@ -20,3 +20,41 @@ const Rectangle = class {
         this.width = width
     }
 }
+
+class Article {
+    constructor(title, date) {
+        this.title = title;
+        this.date= date;
+    }
+
+    //static method
+    static compare(articleA, articleB) {
+        return articleA.date - articleB.date
+    }
+}
+
+//usage
+let article =[
+    new Article('HTML', new Date(2019, 1, 1)),
+    new Article('CSS', new Date(2019, 0, 1)),
+    new Article('JavaScript', new Date(2019, 11, 1))
+];
+
+article.sort(Article.compare)
+
+class ClassWithPrivateField {
+    #privateField;
+
+    constructor() {
+        this.#privateField = 42;
+    }
+}
+
+class Subclass extends ClassWithPrivateField {
+    #subPrivateField;
+    constructor() {
+        super();
+        this.#subPrivateField = 23;
+    }
+}
+new Subclass();
