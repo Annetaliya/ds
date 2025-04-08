ab81caa837a7bd9c8a180b4b2dedca9335ffaeb8
Annette <annetaliya@gmail.com>
2025-04-08 09:35:00 -0700

    common occurence

--- occurrence.js
+++ occurrence.js
@@ -41,4 +41,51 @@ function frequent(array) {
     }
     return occurence;
 }
-console.log(frequent([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]))
+
+function findFrequent (array) {
+    if (array.length < 0) {
+        return null
+    }
+
+    if (!Array.isArray(array)) {
+        return 'Invalid Input'
+    }
+
+    const obj = {};
+    let values = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (obj[array[i]]) {
+            obj[array[i]] += 1
+            
+        } else {
+            obj[array[i]] = 1
+        }
+
+    }
+    
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            values.push(obj[key])
+
+        }
+    } 
+    let max = values[0];
+    for (let i = 1; i < values.length; i++) {
+        if  (values[i] > max){
+            max = values[i];
+        }
+    }
+
+    for (let key in obj) {
+        if (obj[key]===max) {
+            return `${key} (${max})`
+        }
+            
+        
+    }
+    return obj
+    
+    
+}
+console.log(findFrequent([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]))
